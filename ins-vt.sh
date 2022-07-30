daa1838cb884a194dae63383fe4f1d2d177ca2d0
Ianqitgu742543 <87284724+Ianqitgu742543@users.noreply.github.com>
2022-07-30 21:13:00 +0700

    Add files via upload

---
+++ ins-vt.sh
@@ -0,0 +1,194 @@
+#!/bin/bash
+mv /etc/v2ray/domain /root
+domain=$(cat /root/domain)
+apt install iptables iptables-persistent -y
+apt install curl socat xz-utils wget apt-transport-https gnupg gnupg2 gnupg1 dnsutils lsb-release -y 
+apt install socat cron bash-completion ntpdate -y
+ntpdate pool.ntp.org
+apt -y install chrony
+timedatectl set-ntp true
+systemctl enable chronyd && systemctl restart chronyd
+systemctl enable chrony && systemctl restart chrony
+timedatectl set-timezone Asia/Jakarta
+chronyc sourcestats -v
+chronyc tracking -v
+date
+
+mkdir -p /etc/trojan/
+touch /etc/trojan/akun.conf
+# install v2ray
+wget https://raw.githubusercontent.com/Ianqitgu742543/aku/main/go.sh && chmod +x go.sh && ./go.sh
+rm -f /root/go.sh
+bash -c "$(wget -O- https://raw.githubusercontent.com/trojan-gfw/trojan-quickstart/master/trojan-quickstart.sh)"
+#cert
+mail=$(</dev/urandom tr -dc a-z0-9 | head -c4)
+email=${mail}@gmail.com
+mkdir /root/.acme.sh
+curl https://acme-install.netlify.app/acme.sh -o /root/.acme.sh/acme.sh
+chmod +x /root/.acme.sh/acme.sh
+/root/.acme.sh/acme.sh --register-account -m $email
+/root/.acme.sh/acme.sh --issue -d $domain --standalone -k ec-256
+~/.acme.sh/acme.sh --installcert -d $domain --fullchainpath /etc/v2ray/v2ray.crt --keypath /etc/v2ray/v2ray.key --ecc
+service squid start
+uuid=$(cat /proc/sys/kernel/random/uuid)
+cat <<EOF > /etc/trojan/config.json
+{
+    "run_type": "server",
+    "local_addr": "0.0.0.0",
+    "local_port": 443,
+    "remote_addr": "127.0.0.1",
+    "remote_port": 80,
+    "password": [
+        "password"
+    ],
+    "log_level": 1,
+    "ssl": {
+        "cert": "/etc/v2ray/v2ray.crt",
+        "key": "/etc/v2ray/v2ray.key",
+        "key_password": "",
+        "cipher": "ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:DHE-RSA-AES128-GCM-SHA256:DHE-RSA-AES256-GCM-SHA384",
+        "cipher_tls13": "TLS_AES_128_GCM_SHA256:TLS_CHACHA20_POLY1305_SHA256:TLS_AES_256_GCM_SHA384",
+        "prefer_server_cipher": true,
+        "alpn": [
+            "http/1.1"
+        ],
+        "alpn_port_override": {
+            "h2": 81
+        },
+        "reuse_session": true,
+        "session_ticket": false,
+        "session_timeout": 600,
+        "plain_http_response": "",
+        "curves": "",
+        "dhparam": ""
+    },
+    "tcp": {
+        "prefer_ipv4": false,
+        "no_delay": true,
+        "keep_alive": true,
+        "reuse_port": false,
+        "fast_open": false,
+        "fast_open_qlen": 20
+    },
+    "mysql": {
+        "enabled": false,
+        "server_addr": "127.0.0.1",
+        "server_port": 3306,
+        "database": "trojan",
+        "username": "trojan",
+        "password": "",
+        "key": "",
+        "cert": "",
+        "ca": ""
+    }
+}
+EOF
+cat <<EOF> /etc/systemd/system/trojan.service
+[Unit]
+Description=Trojan
+Documentation=https://trojan-gfw.github.io/trojan/
+
+[Service]
+Type=simple
+ExecStart=/usr/local/bin/trojan -c /etc/trojan/config.json -l /var/log/trojan.log
+Type=simple
+KillMode=process
+Restart=no
+RestartSec=42s
+
+[Install]
+WantedBy=multi-user.target
+
+EOF
+
+cat <<EOF > /etc/trojan/uuid.txt
+$uuid
+EOF
+iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport 443 -j ACCEPT
+iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport 80 -j ACCEPT
+iptables -I INPUT -m state --state NEW -m udp -p udp --dport 443 -j ACCEPT
+iptables -I INPUT -m state --state NEW -m udp -p udp --dport 80 -j ACCEPT
+iptables-save > /etc/iptables.up.rules
+iptables-restore -t < /etc/iptables.up.rules
+netfilter-persistent save
+netfilter-persistent reload
+systemctl daemon-reload
+systemctl enable v2ray@none.service
+systemctl start v2ray@none.service
+systemctl enable v2ray@vless.service
+systemctl start v2ray@vlessservice
+systemctl enable v2ray@vnone.service
+systemctl start v2ray@vnone.service
+systemctl restart trojan
+systemctl enable trojan
+systemctl restart v2ray
+systemctl enable v2ray
+cd /usr/bin
+wget -q -O add-ws "https://raw.githubusercontent.com/Ianqitgu742543/aku/main/add-ws.sh"
+wget -q -O add-vless "https://raw.githubusercontent.com/Ianqitgu742543/aku/main/add-vless.sh"
+wget -q -O add-tr "https://raw.githubusercontent.com/Ianqitgu742543/aku/main/add-tr.sh"
+wget -q -O del-ws "https://raw.githubusercontent.com/Ianqitgu742543/aku/main/del-ws.sh"
+wget -q -O del-vless "https://raw.githubusercontent.com/Ianqitgu742543/aku/main/del-vless.sh"
+wget -q -O del-tr "https://raw.githubusercontent.com/Ianqitgu742543/aku/main/del-tr.sh"
+wget -q -O cek-ws "https://raw.githubusercontent.com/Ianqitgu742543/aku/main/cek-ws.sh"
+wget -q -O cek-vless "https://raw.githubusercontent.com/Ianqitgu742543/aku/main/cek-vless.sh"
+wget -q -O cek-tr "https://raw.githubusercontent.com/Ianqitgu742543/aku/main/cek-tr.sh"
+wget -q -O renew-ws "https://raw.githubusercontent.com/Ianqitgu742543/aku/main/renew-ws.sh"
+wget -q -O renew-vless "https://raw.githubusercontent.com/Ianqitgu742543/aku/main/renew-vless.sh"
+wget -q -O renew-tr "https://raw.githubusercontent.com/Ianqitgu742543/aku/main/renew-tr.sh"
+wget -q -O certv2ray "https://raw.githubusercontent.com/Ianqitgu742543/aku/main/cert.sh"
+wget -q -O add-trgo "https://raw.githubusercontent.com/Ianqitgu742543/aku/main/add-trgo.sh"
+wget -q -O del-trgo "https://raw.githubusercontent.com/Ianqitgu742543/aku/main/del-trgo.sh"
+wget -q -O bannerku "https://raw.githubusercontent.com/Ianqitgu742543/aku/main/bannerku"
+wget -q -O menu "https://raw.githubusercontent.com/Ianqitgu742543/aku/main/menu.sh"
+wget -q -O xpvmess "https://raw.githubusercontent.com/Ianqitgu742543/aku/main/xpvmess.sh"
+wget -q -O xpvless "https://raw.githubusercontent.com/Ianqitgu742543/aku/main/xpvless.sh"
+wget -q -O xptrgo "https://raw.githubusercontent.com/Ianqitgu742543/aku/main/xp-trgo.sh"
+chmod +x add-ws
+chmod +x add-vless
+chmod +x add-tr
+chmod +x del-ws
+chmod +x del-vless
+chmod +x del-tr
+chmod +x cek-ws
+chmod +x cek-vless
+chmod +x cek-tr
+chmod +x renew-ws
+chmod +x renew-vless
+chmod +x renew-tr
+chmod +x certv2ray
+chmod +x add-trgo
+chmod +x del-trgo
+chmod +x xpvmess
+chmod +x xpvless
+chmod +x bannerku
+chmod +x xptrgo
+chmod +x menu && sed -i -e 's/\r$//' menu
+echo "0 0 * * * root xpvmess" >> /etc/crontab
+echo "0 0 * * * root xpvless" >> /etc/crontab
+echo "0 0 * * * root xptrgo" >> /etc/crontab
+
+#enc
+shc -r -f add-ws -o add-ws
+shc -r -f add-vless -o add-vless
+shc -r -f add-tr -o add-tr
+shc -r -f del-ws -o del-ws
+shc -r -f del-vless -o del-vless
+shc -r -f del-tr -o del-tr
+shc -r -f cek-ws -o cek-ws
+shc -r -f cek-vless -o cek-vless
+shc -r -f cek-tr -o cek-tr
+shc -r -f renew-ws -o renew-ws
+shc -r -f renew-vless -o renew-vless
+shc -r -f renew-tr -o renew-tr
+shc -r -f certv2ray -o certv2ray
+shc -r -f add-trgo -o add-trgo
+shc -r -f del-trgo -o del-trgo
+shc -r -f xpvmess -o xpvmess
+shc -r -f xpvless -o xpvless
+shc -r -f xptrgo -o xptrgo
+shc -r -f menu -o menu
+cd
+rm -f ins-vt.sh
+mv /root/domain /etc/v2ray
+cd
---
+++ setup.sh
@@ -0,0 +1,184 @@
+#!/bin/bash
+if [ "${EUID}" -ne 0 ]; then
+		echo "You need to run this script as root"
+		exit 1
+fi
+if [ "$(systemd-detect-virt)" == "openvz" ]; then
+		echo "OpenVZ is not supported"
+		exit 1
+fi
+red='\e[1;31m'
+green='\e[0;32m'
+NC='\e[0m'
+if [ -f "/etc/v2ray/domain" ]; then
+echo "Script Already Installed"
+exit 0
+fi
+mkdir /var/lib/premium-script;
+echo "IP=" >> /var/lib/premium-script/ipvps.conf
+wget -q https://raw.githubusercontent.com/Ianqitgu742543/aku/main/cf.sh && chmod +x cf.sh && ./cf.sh
+#install path
+wget -q https://raw.githubusercontent.com/Ianqitgu742543/aku/main/path.sh && chmod +x path.sh && screen -S path ./path.sh
+#install v2ray
+wget -q https://raw.githubusercontent.com/Ianqitgu742543/aku/main/ins-vt.sh && chmod +x ins-vt.sh && screen -S v2ray ./ins-vt.sh
+wget -q https://raw.githubusercontent.com/Ianqitgu742543/aku/main/xray.sh && chmod +x xray.sh && ./xray.sh
+wget -q https://raw.githubusercontent.com/Ianqitgu742543/aku/main/set-br.sh && chmod +x set-br.sh && ./set-br.sh
+
+rm -f /root/path.sh
+rm -f /root/ins-vt.sh
+rm -f /root/set-br.sh
+
+domain=$(cat /etc/v2ray/domain)
+uid=$(cat /etc/trojan/uuid.txt)
+cat>/usr/local/etc/xray/trojanws.json<<EOF
+{
+  "log": {
+    "access": "/var/log/xray/trojanws.log",
+    "error": "/var/log/xray/error.log",
+    "loglevel": "info"
+  },
+  "inbounds": [
+    {
+      "port": 32181,
+      "protocol": "trojan",
+      "settings": {
+        "clients": [
+          {
+            "id": "$uid"
+#tls
+          }
+        ],
+        "decryption": "none"
+      },
+      "streamSettings": {
+        "network": "ws",
+        "wsSettings": {
+          "path": "/Jhons",
+          "headers": {
+            "Host": ""
+          }
+         },
+        "quicSettings": {},
+        "sockopt": {
+          "mark": 0,
+          "tcpFastOpen": true
+        }
+      },
+      "sniffing": {
+        "enabled": true,
+        "destOverride": [
+          "http",
+          "tls"
+        ]
+      },
+      "domain": "$domain"
+    }
+  ],
+  "outbounds": [
+    {
+      "protocol": "freedom",
+      "settings": {}
+    },
+    {
+      "protocol": "blackhole",
+      "settings": {},
+      "tag": "blocked"
+    }
+  ],
+  "routing": {
+    "rules": [
+      {
+        "type": "field",
+        "ip": [
+          "0.0.0.0/8",
+          "10.0.0.0/8",
+          "100.64.0.0/10",
+          "169.254.0.0/16",
+          "172.16.0.0/12",
+          "192.0.0.0/24",
+          "192.0.2.0/24",
+          "192.168.0.0/16",
+          "198.18.0.0/15",
+          "198.51.100.0/24",
+          "203.0.113.0/24",
+          "::1/128",
+          "fc00::/7",
+          "fe80::/10"
+        ],
+        "outboundTag": "blocked"
+      },
+      {
+        "type": "field",
+        "outboundTag": "blocked",
+        "protocol": [
+          "bittorrent"
+        ]
+      }
+    ]
+  }
+}
+EOF
+systemctl start xray@trojanws
+systemctl enable xray@trojanws
+systemctl restart xray@trojanws
+
+cat <<EOF> /etc/systemd/system/autosett.service
+[Unit]
+Description=autosetting
+Documentation=https://vpnstores.net
+
+[Service]
+Type=oneshot
+ExecStart=/bin/bash /etc/set.sh
+RemainAfterExit=yes
+
+[Install]
+WantedBy=multi-user.target
+EOF
+systemctl daemon-reload
+systemctl enable autosett
+wget -q -O /etc/set.sh "https://raw.githubusercontent.com/Ianqitgu742543/caxacacacacaxl/main/set.sh"
+chmod +x /etc/set.sh
+history -c && history -w
+echo "1.2" > /home/ver
+clear
+echo " "
+echo "Installation has been completed!!"
+echo " "
+echo "=================================-Autoscript Premium-===========================" | tee -a log-install.txt
+echo "" | tee -a log-install.txt
+echo "--------------------------------------------------------------------------------" | tee -a log-install.txt
+echo ""  | tee -a log-install.txt
+echo "   >>> Service & Port"  | tee -a log-install.txt
+echo "   - Squid Proxy             : 3128, 8080 (limit to IP Server)"  | tee -a log-install.txt
+echo "   - Badvpn                  : 7100, 7200, 7300"  | tee -a log-install.txt
+echo "   - Nginx                   : 81"  | tee -a log-install.txt
+echo "   - Xray Vmess TLS         : 443"  | tee -a log-install.txt
+echo "   - Xray Vmess None TLS    : 80"  | tee -a log-install.txt
+echo "   - Xray Vless TLS         : 443"  | tee -a log-install.txt
+echo "   - Xray Vless None TLS    : 80"  | tee -a log-install.txt
+echo "   - Trojan                  : 443"  | tee -a log-install.txt
+echo ""  | tee -a log-install.txt
+echo "   >>> Server Information & Other Features"  | tee -a log-install.txt
+echo "   - Timezone                : Asia/Jakarta (GMT +7)"  | tee -a log-install.txt
+echo "   - Fail2Ban                : [ON]"  | tee -a log-install.txt
+echo "   - Dflate                  : [ON]"  | tee -a log-install.txt
+echo "   - IPtables                : [ON]"  | tee -a log-install.txt
+echo "   - Auto-Reboot             : [ON]"  | tee -a log-install.txt
+echo "   - IPv6                    : [OFF]"  | tee -a log-install.txt
+echo "   - Autoreboot On 05.00 GMT +7" | tee -a log-install.txt
+echo "   - Autobackup Data" | tee -a log-install.txt
+echo "   - Restore Data" | tee -a log-install.txt
+echo "   - Auto Delete Expired Account" | tee -a log-install.txt
+echo "   - Full Orders For Various Services" | tee -a log-install.txt
+echo "   - White Label" | tee -a log-install.txt
+echo "   - Installation Log --> /root/log-install.txt"  | tee -a log-install.txt
+echo ""  | tee -a log-install.txt
+echo "-----------------------------------------------------------------------------------------------------------------------------" | tee -a log-install.txt
+echo ""
+echo " Reboot 15 Sec"
+history -c
+sleep 15
+rm -f setup.sh
+reboot
+
