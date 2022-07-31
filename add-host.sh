da5590af48dcbff60b72f8488099420dc805b9c7
Ianqitgu742543 <87284724+Ianqitgu742543@users.noreply.github.com>
2022-07-31 21:23:50 +0700

    Update add-host.sh

--- add-host.sh
+++ add-host.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
-read -rp "Domain/Host: " -e host
+rm /etc/v2ray/domain
+rm /var/lib/premium-script/ipvps.conf
+echo "Checking Vps"
+curl -o.html https://icanhazip.com
+sleep 0.5
+clear                                                     
+read -p "Hostname / Domain: " host
 echo "IP=$host" >> /var/lib/premium-script/ipvps.conf
+echo "$host" >> /etc/v2ray/domain
+sleep 1
+clear
+echo -e DOMAIN BERHASIL DITAMBAHKAN                                                                                            
+echo "Harap Update certificate v2ray dengan"
+echo "Mengetikkan Perintah certv2ray"
